ad6c3f8b5fecd317819d7dabe5bd4515b77b975b
Wilson Yu <wilsonyu1709@gmail.com>
2021-02-13 23:49:27 +1000

    implement additional degrees

--- src/components/RequestNewDegree.tsx
+++ src/components/RequestNewDegree.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import SelectionGridElement from "./SelectionGridElement";
+import './styles/RequestNewDegree.css';
 
 const RequestNewDegree = (props : any) => {
     return (
         <div>
-            
+            <div>
+                <div className = "requestHeadline">
+                    Select another degree?
+                </div>
+                <div className = "requestExplain"> 
+                    This option is intended for degree structures such as diplomas, Education and dual degrees where different degrees are studied concurrently. 
+                </div>
+            </div>
+            <div className = "requestButtons">
+                <SelectionGridElement className = "degreeSelection" user = {props.user} onClick = { props.handler } 
+                name = "Yes" element = {true} />
+                <SelectionGridElement className = "degreeSelection" user = {props.user} onClick = { props.handler } 
+                name = "No" element = {false} />
+            </div>
         </div>
     )
 }
