b28878c71c6e6ad6d50af76aa2c6011bffff75ae
Wilson Yu <wilsonyu1709@gmail.com>
2021-11-23 16:40:32 +1000

    Convert back to using CodeDeploy

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 cd /home/ec2-user/uq_degree_planner/
-docker-compose up --build -d
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
+docker pull public.ecr.aws/r8i2f5k8/uq_degree_planner:latest
+docker run -d --name uq_degree_planner --env-file .env --net=host public.ecr.aws/r8i2f5k8/uq_degree_planner:latest
---
+++ stop.sh
@@ -0,0 +1,2 @@
+cd /home/ec2-user/uq_degree_planner/
+docker stop uq_degree_planner
